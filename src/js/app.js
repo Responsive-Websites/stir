5bc7fc0ae05c7b091d4ff510b9a0e70ae1afa7a1
Danila-hub <kontaigabox@mail.ru>
2022-07-04 13:36:04 +0300

    Added some fixes for team slider and finish burger styles

--- src/js/app.js
+++ src/js/app.js
@@ -92,6 +92,34 @@ if (menuLinks.length > 0) {
   }
 }
 
+const menuBurgerLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuBurgerLinks.length > 0) {
+  menuBurgerLinks.forEach((menuLink) => {
+    menuLink.addEventListener('click', onMenuLinkClick);
+  });
+
+  function onMenuLinkClick(e) {
+    const menuLink = e.target;
+    if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+      const gotoBlock = document.querySelector(menuLink.dataset.goto);
+      const gotoBlockValue =
+        gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+      if (burger_icon.classList.contains('_active')) {
+        document.body.classList.remove('_lock');
+        burger_icon.classList.remove('_active');
+        header_menu.classList.remove('_active');
+      }
+
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: 'smooth',
+      });
+      e.preventDefault();
+    }
+  }
+}
+
 // =========================================================
 
 // Dynamic Adaptive
